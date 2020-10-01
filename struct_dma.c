03248eedc4f44e1da29b3626eafae5b6faff5635
ANURAG PRATIK <72085738+anurag-pratik@users.noreply.github.com>
2020-10-01 10:58:47 +0530

    Update struct_dma.c
    changes in print statements

--- struct_dma.c
+++ struct_dma.c
@@ -12,27 +12,27 @@ int main()
 {
     struct employee *emp;
     int n,i;
-    printf("enter the no of records to be stored");
+    printf("Enter the no of records to be stored: ");
     scanf("%d",&n);
     emp=(struct employee *)malloc(n*sizeof(struct employee));
     if(emp==NULL)
     printf("error insufficient memory");
     else
-    printf("memory allocated successfully \n");
+    printf("Memory allocated successfully \n");
     //( taking inputs)//
     for(i=0; i<n; i++)
     {
-     printf("enter the employee number\n");
+     printf("Enter the employee number: \n");
      scanf("%d",&(emp+i)->eno);
-     printf("enter the name of the employee\n");
+     printf("Enter the name of the employee: \n");
      scanf("%s",(emp+i)->ename);
-     printf("enter the salary of the employee\n");
+     printf("Enter the salary of the employee: \n");
      scanf("%d",&(emp+i)->empsal);
     }
     //(printing the info of the employees)//
     for(i=0; i<n;i++)
     {
-     printf("the name of the employee is= %s \n the employee no is= %d \n the salary of the employee is %d\n",(emp+i)->ename,(emp+i)->eno,(emp+i)->empsal);
+     printf("The name of the employee is= %s \n The employee no is= %d \n The salary of the employee is: %d\n",(emp+i)->ename,(emp+i)->eno,(emp+i)->empsal);
     }
     return (0);
         
